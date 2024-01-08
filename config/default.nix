50a243038baa89ff219f7afb1fabbe2bf8e11a49
Xavier Ruiz <github@xav.ie>
2024-01-08 05:49:59 -0600

    feat(base setup): pretty good starter setup that can be improved over time

--- config/default.nix
+++ config/default.nix
@@ -1,6 +1,423 @@
-{
+{pkgs, ...}: {
+  # TODO:
+  # [ ] checkout ts-auto-tag:
+  #     https://github.com/pta2002/nixos-config/blob/main/modules/nvim.nix
+  # [ ] checkout these other plugins:
+  # 	https://github.com/Alexnortung/nollevim/blob/fcc35456c567c6108774e839d617c97832217e67/config/appearance/treesitter.nix#L2
+  # [ ] lsp code actions with telescope?
+  #	https://github.com/nvim-telescope/telescope-ui-select.nvim
+  # 	https://github.com/traxys/nvim-flake/blob/c753bb1e624406ef454df9e8cb59d0996000dc93/config.nix#L306
+  # [ ] read through these config(s) fully:
+  # 	https://github.com/Builditluc/dotfiles/blob/0989f7bf0d147232b4133d9fe4fb166465e93b94/dotnix/hm/nvim.nix#L95
+  # 	https://github.com/pta2002/nixos-config/blob/main/modules/nvim.nix
+  #	https://github.com/evccyr/dotfiles/blob/main/nix/neovim/default.nix
+  # [ ] would git blame floating messages be helpful? https://github.com/rhysd/git-messenger.vim
+  # [ ] what about clipboard management in vim? https://github.com/gbprod/yanky.nvim
+  # [ ] is lspsaga any good? looks kind of bloated and feature creeped.
+  # [ ] write my own or find someone else's snippets for ts. specifically all the map, reduce, forEach should have snippets
+  # 	https://www.youtube.com/watch?v=Dn800rlPIho
+  # 	https://github.com/L3MON4D3/LuaSnip#resources-for-new-users
+  # [ ] harpoon?
+  # [ ] flash nvim for faster jumping
+  # [ ] eslint with conform? or none-ls? idk the real differences yet
+
+
   # Import all your configuration modules here
   imports = [
     ./bufferline.nix
   ];
+  config = {
+    colorschemes = {
+      base16 = {
+        enable = true;
+        #colorscheme = "pinky";
+        #colorscheme = "colors";
+        #colorscheme = "purpledream";
+        # somehow, gatekeeper theme from nvchad is based on pico
+        # but this looks bad, but I love gatekeeper
+        #colorscheme = "pico";
+        customColorScheme = {
+          # background
+          base00 = "NONE";
+          # active tab, ui tab foreground
+          base01 = "#603090";
+          # highlight text color
+          base02 = "#1060ee";
+          # comments
+          base03 = "#a090a0";
+          # line number color
+          base04 = "#a0a0ff";
+          # special characters ()=>{}
+          base05 = "#e0e0e0";
+          # ???
+          base06 = "#00ff00";
+          # ???
+          base07 = "#a0a000";
+          # html tags, variables, attributes, ui tab background
+          base08 = "#FFB20F";
+          # values
+          base09 = "#00a0f0";
+          # types, html attributes
+          base0A = "#BE620A";
+          # strings, unsaved tab, ui tab background
+          base0B = "#00E756";
+          # ???
+          base0C = "#FFD242";
+          # values
+          base0D = "#9A5FEB";
+          # keywords, function calls
+          base0E = "#ff3a8e";
+          # special characters, semi-colons, commas
+          base0F = "#f06949";
+        };
+      };
+    };
+
+    extraConfigLua = ''
+      vim.opt.cmdheight = 0;
+
+      -- add border to diagnostic windows
+      local _border = "single"
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+        border = _border,
+      })
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+        border = _border,
+      })
+      vim.diagnostic.config {
+        float = { border = _border },
+      }
+    '';
+
+    extraPlugins = with pkgs.vimPlugins; [
+      friendly-snippets
+    ];
+
+    globals = {
+      # merge statusbar into command bar
+      # ...except, this does not work :/
+      # cmdheight = 0;
+      mapleader = " ";
+      # 24-bit colors
+      termguicolors = true;
+      # don't care about case when search
+      ignorecase = true;
+      # ...unless you used uppercase
+      smartcase = true;
+    };
+
+    # TODO: clean this up
+    highlight = {
+      ColorColumn = {
+        underline = true;
+      };
+
+      PmenuSel = { bg = "#504945"; fg = "NONE"; };
+      Pmenu = { fg = "#ebdbb2"; bg = "#282828"; };
+
+      
+      CmpItemAbbrDeprecated = { fg = "#d79921"; bg = "NONE"; strikethrough = true; };
+      CmpItemAbbrMatch = { fg = "#83a598"; bg = "NONE"; bold = true; };
+      CmpItemAbbrMatchFuzzy = { fg = "#83a598"; bg = "NONE"; bold = true; };
+      CmpItemMenu = { fg = "#b16286"; bg = "NONE"; italic = true; };
+
+      CmpItemKindField = { fg = "#fbf1c7"; bg = "#fb4934"; };
+      CmpItemKindProperty = { fg = "#fbf1c7"; bg = "#fb4934"; };
+      CmpItemKindEvent = { fg = "#fbf1c7"; bg = "#fb4934"; };
+
+      CmpItemKindText = { fg = "#fbf1c7"; bg = "#b8bb26"; };
+      CmpItemKindEnum = { fg = "#fbf1c7"; bg = "#b8bb26"; };
+      CmpItemKindKeyword = { fg = "#fbf1c7"; bg = "#b8bb26"; };
+
+      CmpItemKindConstant = { fg = "#fbf1c7"; bg = "#fe8019"; };
+      CmpItemKindConstructor = { fg = "#fbf1c7"; bg = "#fe8019"; };
+      CmpItemKindReference = { fg = "#fbf1c7"; bg = "#fe8019"; };
+
+      CmpItemKindFunction = { fg = "#fbf1c7"; bg = "#b16286"; };
+      CmpItemKindStruct = { fg = "#fbf1c7"; bg = "#b16286"; };
+      CmpItemKindClass = { fg = "#fbf1c7"; bg = "#b16286"; };
+      CmpItemKindModule = { fg = "#fbf1c7"; bg = "#b16286"; };
+      CmpItemKindOperator = { fg = "#fbf1c7"; bg = "#b16286"; };
+
+      CmpItemKindVariable = { fg = "#fbf1c7"; bg = "#458588"; };
+      CmpItemKindFile = { fg = "#fbf1c7"; bg = "#458588"; };
+
+      CmpItemKindUnit = { fg = "#fbf1c7"; bg = "#d79921"; };
+      CmpItemKindSnippet = { fg = "#fbf1c7"; bg = "#d79921"; };
+      CmpItemKindFolder = { fg = "#fbf1c7"; bg = "#d79921"; };
+
+      CmpItemKindMethod = { fg = "#fbf1c7"; bg = "#8ec07c"; };
+      CmpItemKindValue = { fg = "#fbf1c7"; bg = "#8ec07c"; };
+      CmpItemKindEnumMember = { fg = "#fbf1c7"; bg = "#8ec07c"; };
+
+      CmpItemKindInterface = { fg = "#fbf1c7"; bg = "#83a598"; };
+      CmpItemKindColor = { fg = "#fbf1c7"; bg = "#83a598"; };
+      CmpItemKindTypeParameter = { fg = "#fbf1c7"; bg = "#83a598"; };
+
+      FloatBorder = { fg = "#a89984"; };
+    };
+
+    keymaps = [
+      {
+        action = "<cmd>Telescope live_grep<CR>";
+        key = "<leader>g";
+      }
+      # Equivalent to nnoremap ; :
+      {
+        action = ":";
+        key = ";";
+      }
+      # Equivalent to nmap <silent> <buffer> <leader>gg <cmd>Man<CR>
+      {
+        action = "<cmd>Man<CR>";
+        key = "<leader>gg";
+        options = {
+          silent = true;
+          remap = false;
+        };
+      }
+    ];
+
+    match = {
+      ColorColumn = "\\%101v";
+    };
+
+    options = {
+      number = true; # Show line numbers
+      relativenumber = true; # Show relative line numbers
+      shiftwidth = 2; # Tab width should be 2
+    };
+
+    plugins = {
+      # TODO: consider barbar.enable instead
+      # tabs
+      bufferline = {
+        enable = true;
+        separatorStyle = "thin";
+        showBufferCloseIcons = false;
+        indicator.icon = "▌";
+      };
+
+      # smart comment/uncomment
+      comment-nvim.enable = true;
+
+      # luasnip expansions in cmp
+      cmp_luasnip.enable = true;
+
+      # the best git plugin
+      fugitive.enable = true;
+
+      # git indicators in the left gutter
+      gitsigns.enable = true;
+
+      # indentation lines ui guides
+      indent-blankline.enable = true;
+
+      # finally, normal behaving tabs
+      intellitab.enable = true;
+
+      lsp = {
+        enable = true;
+        keymaps = {
+          silent = true;
+          diagnostic = {
+            # Navigate in diagnostics
+            "[d" = "goto_prev";
+            "]d" = "goto_next";
+            "<leader>d" = "open_float";
+          };
+
+          lspBuf = {
+            gd = "definition";
+            gD = "references";
+            gt = "type_definition";
+            gi = "implementation";
+            K = "hover";
+            "<leader>r" = "rename";
+          };
+        };
+        servers = {
+          nixd.enable = true;
+          eslint.enable = true;
+          tsserver.enable = true;
+          lua-ls.enable = true;
+          rust-analyzer = {
+            enable = true;
+            installCargo = true;
+            installRustc = true;
+          };
+        };
+      };
+
+      # completion icons
+      lspkind = {
+        enable = true;
+        cmp = {
+          enable = true;
+        };
+      };
+
+      # better diagnostics ui
+      lsp-lines = {
+        enable = true;
+        # currentLine = true;
+      };
+
+      # statusline
+      lualine = {
+        enable = true;
+        theme = "powerline_dark";
+        # TODO: figure out the components
+        # https://github.com/nvim-lualine/lualine.nvim/blob/566b7036f717f3d676362742630518a47f132fff/examples/evil_lualine.lua
+      };
+
+      # useful code expansions
+      luasnip = {
+	enable = true;
+	extraConfig = {
+#	  enable_autosnippets = true;
+#	  store_selection_keys = “<Tab>”;
+	};
+	fromVscode = [{}];
+      };
+
+      # TODO: figure out if this is possible with just treesitter?
+      # nix.enable = true;
+
+      noice = {
+        enable = true;
+        messages = {
+          view = "mini";
+          viewError = "mini";
+          viewWarn = "mini";
+        };
+
+        lsp.override = {
+          "vim.lsp.util.convert_input_to_markdown_lines" = true;
+          "vim.lsp.util.stylize_markdown" = true;
+          "cmp.entry.get_documentation" = true;
+        };
+
+        presets = {
+          bottom_search = true;
+          command_palette = true;
+          long_message_to_split = true;
+          inc_rename = true;
+          lsp_doc_border = true;
+        };
+      };
+
+      # ()[]{}...
+      nvim-autopairs.enable = true;
+
+      # completions
+      nvim-cmp = {
+        enable = true;
+        autoEnableSources = true;
+        sources = [
+          {name = "nvim_lsp";}
+          {name = "path";}
+          {
+            name = "buffer";
+            # Words from other open buffers can also be suggested.
+            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+          }
+          {name = "luasnip";}
+          # TODO: {name = "neorg";}
+        ];
+
+        mapping = {
+          "<C-u>" = "cmp.mapping.scroll_docs(-3)";
+          "<C-d>" = "cmp.mapping.scroll_docs(3)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<tab>" = "cmp.mapping.close()";
+          "<c-n>" = {
+            modes = ["i" "s"];
+            action = "cmp.mapping.select_next_item()";
+          };
+          "<c-p>" = {
+            modes = ["i" "s"];
+            action = "cmp.mapping.select_prev_item()";
+          };
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+        };
+
+        snippet.expand = "luasnip";
+      };
+
+      # colors in neovim
+      nvim-colorizer.enable = true;
+
+      # TODO: figure out if this is good or not
+      # vscode lightbulbs
+      # nvim-lightbulb = {
+      #   enable = true;
+      #   autocmd.enabled = true;
+      # };
+
+      # better folding ui
+      #nvim-ufo.enable = true;
+
+      # easily browse directories
+      oil.enable = true;
+
+      # tpope === goat
+      surround.enable = true;
+
+      # fzf client
+      telescope = {
+        enable = true;
+        defaults = {
+          file_ignore_patterns = [
+            "^.git/"
+            "^.mypy_cache/"
+            "^__pycache__/"
+            "%.ipynb"
+          ];
+          set_env.COLORTERM = "truecolor";
+        };
+      };
+
+      todo-comments.enable = true;
+
+      # ast syntax highlighting
+      treesitter = {
+        enable = true;
+        # TODO: figure out how to make this not open files pre-folded
+        # fold based on ast
+        # folding = true;
+
+        # indent based on ast
+        indent = true;
+        # lua highlighting for nixvim lua sections
+        nixvimInjections = true;
+        # this is SO useful
+        incrementalSelection = {
+          enable = true;
+          keymaps = {
+            initSelection = "<C-space>";
+            nodeDecremental = "<bs>";
+            nodeIncremental = "<C-space>";
+            # idk what this does
+            scopeIncremental = "grc";
+          };
+        };
+      };
+
+      # diagnostics buffer
+      trouble.enable = true;
+
+      # even better %
+      vim-matchup = {
+        treesitterIntegration = {
+          enable = true;
+          includeMatchWords = true;
+        };
+        enable = true;
+      };
+
+      # TODO: see https://github.com/Alexnortung/nollevim/blob/fcc35456c567c6108774e839d617c97832217e67/config/which-key.nix#L4
+      which-key.enable = true;
+    };
+  };
 }
