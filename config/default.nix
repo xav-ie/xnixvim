2c39664ea9236aee67a205fe99e179802f14e8a1
xav-ie <github@xav.ie>
2024-04-17 18:30:36 -0400

    fix(gitsigns): register binds through which-key for better command descriptions

--- config/default.nix
+++ config/default.nix
@@ -538,31 +538,39 @@ in
                   vim.keymap.set(mode, l, r, opts)
                 end
 
-                -- Navigation
-                map('n', ']c', function()
-                  if vim.wo.diff then return ']c' end
-                  vim.schedule(function() gs.next_hunk() end)
-                  return '<Ignore>'
-                end, {expr=true})
-
-                map('n', '[c', function()
-                  if vim.wo.diff then return '[c' end
-                  vim.schedule(function() gs.prev_hunk() end)
-                  return '<Ignore>'
-                end, {expr=true})
-
-                -- Actions
-                map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-                map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-                map('n', '<leader>hS', gs.stage_buffer)
-                map('n', '<leader>hu', gs.undo_stage_hunk)
-                map('n', '<leader>hR', gs.reset_buffer)
-                map('n', '<leader>hp', gs.preview_hunk)
-                map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-                map('n', '<leader>tb', gs.toggle_current_line_blame)
-                map('n', '<leader>hd', gs.diffthis)
-                map('n', '<leader>hD', function() gs.diffthis('~') end)
-                map('n', '<leader>td', gs.toggle_deleted)
+                -- Register key mappings and descriptions using which-key
+                local wk = require("which-key")
+                wk.register({
+                    ["]c"] = { function()
+                        if vim.wo.diff then return "]c" end
+                        vim.schedule(function() gs.next_hunk() end)
+                        return '<Ignore>'
+                    end, "Next Hunk" },
+                    ["[c"] = { function()
+                        if vim.wo.diff then return "[c" end
+                        vim.schedule(function() gs.prev_hunk() end)
+                        return '<Ignore>'
+                    end, "Previous Hunk" },
+                    ["<leader>"] = {
+                        h = {
+                            name = "Gitsigns",
+                            s = { ":Gitsigns stage_hunk<CR>", "Stage Hunk" },
+                            r = { ":Gitsigns reset_hunk<CR>", "Reset Hunk" },
+                            S = { gs.stage_buffer, "Stage Buffer" },
+                            u = { gs.undo_stage_hunk, "Undo Stage Hunk" },
+                            R = { gs.reset_buffer, "Reset Buffer" },
+                            p = { gs.preview_hunk, "Preview Hunk" },
+                            b = { function() gs.blame_line{full=true} end, "Blame Full Line" },
+                            d = { gs.diffthis, "Diff This" },
+                            D = { function() gs.diffthis('~') end, "Diff This ~" },
+                        },
+                        t = {
+                            name = "Toggle",
+                            b = { gs.toggle_current_line_blame, "Toggle Line Blame" },
+                            d = { gs.toggle_deleted, "Toggle Deleted" },
+                        }
+                    }
+                }, { mode = "n", buffer = bufnr, silent = true, noremap = true, nowait = true })
 
                 -- Text object
                 map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
