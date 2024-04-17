e284bd3cc11d29d06895d31c4e324ba36fcc9c82
xav-ie <github@xav.ie>
2024-04-17 17:38:31 -0400

    format

--- flake.nix
+++ flake.nix
@@ -2,20 +2,34 @@
   description = "A nixvim configuration";
 
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixvim.url = "github:nix-community/nixvim";
-    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { nixvim, flake-parts, ... }@inputs:
-    let config = import ./config; # import the module directly
-    in flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs =
+    {
+      nixvim,
+      flake-parts,
+      neovim-nightly-overlay,
+      ...
+    }@inputs:
+    let
+      config = import ./config; # import the module directly
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
       # TODO: copy this excellent nixvim config:
       # https://github.com/asungy/nixim/blob/979643e6a13d1803915b379a0e974b86cbcd77ea/flake.nix
-      systems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
 
-      perSystem = { pkgs, system, ... }:
+      perSystem =
+        { pkgs, system, ... }:
         let
           nixvimLib = nixvim.lib.${system};
           nixvim' = nixvim.legacyPackages.${system};
@@ -25,10 +39,12 @@
             # You can use `extraSpecialArgs` to pass additional arguments to your module files
             # idk how to use this
             extraSpecialArgs = {
+              inherit neovim-nightly-overlay;
               # inherit (inputs) foo;
             };
           };
-        in {
+        in
+        {
           checks = {
             # Run `nix flake check .` to verify that your config is not broken
             default = nixvimLib.check.mkTestDerivationFromNvim {
