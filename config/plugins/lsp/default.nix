b21dec74c2de3214298ffd19d827f0b6488a3959
xav-ie <github@xav.ie>
2024-06-22 16:36:08 -0400

    config(lsp.diagnostic): simplify config
    I can't find any effect with the other settings.

--- config/plugins/lsp/default.nix
+++ config/plugins/lsp/default.nix
@@ -2,18 +2,12 @@
 {
   imports = [ ./SchemaStore-nvim.nix ];
 
-  extraConfigLua = # lua
+  extraConfigLua =
+    # lua
     ''
       -- add border to diagnostic windows
-      local _border = "single"
-      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-        border = _border,
-      })
-      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-        border = _border,
-      })
       vim.diagnostic.config {
-        float = { border = _border },
+        float = { border = "single" },
       }
     '';
 
