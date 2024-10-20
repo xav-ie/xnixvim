63d741ce219b9637115bfea48da3e9152486a565
xav-ie <github@xav.ie>
2024-10-19 23:08:04 -0400

    fix(highlight): move highlight to correct location

--- config/highlight.nix
+++ config/highlight.nix
@@ -3,19 +3,6 @@
   config = {
     # TODO: clean this up
     highlight = {
-      # ColorColumn = {
-      #   underline = true;
-      # };
-
-      PmenuSel = {
-        bg = "#504945";
-        fg = "NONE";
-      };
-      Pmenu = {
-        fg = "#ebdbb2";
-        bg = "#282828";
-      };
-
       CmpItemAbbrDeprecated = {
         fg = "#d79921";
         bg = "NONE";
@@ -144,13 +131,33 @@
         fg = "#fbf1c7";
         bg = "#83a598";
       };
+      # ColorColumn = {
+      #   underline = true;
+      # };
 
       FloatBorder = {
         fg = "#a89984";
       };
-
+      PmenuSel = {
+        bg = "#504945";
+        fg = "NONE";
+      };
+      Pmenu = {
+        fg = "#ebdbb2";
+        bg = "#282828";
+      };
       TabLineFill = {
+        # tab bar background
         bg = "NONE";
+      };
+      TSProperty = {
+        fg = "#FFD242";
+      };
+      TSType = {
+        fg = "#00a0f0";
+      };
+      TSNumber = {
+        fg = "#be620a";
       };
     };
   };
