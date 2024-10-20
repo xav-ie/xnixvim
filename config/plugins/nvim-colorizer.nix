40dd26e297362ad326eae6569f736a6df800a7c2
xav-ie <github@xav.ie>
2024-10-20 01:41:01 -0400

    fix(colorizer): highlight all possible colors

---
+++ config/plugins/nvim-colorizer.nix
@@ -0,0 +1,21 @@
+{ ... }:
+{
+  # colors in Neovim
+  # https://github.com/norcalli/nvim-colorizer.lua
+  # https://nix-community.github.io/nixvim/plugins/nvim-colorizer/index.html
+  config = {
+    plugins.nvim-colorizer = {
+      enable = true;
+      userDefaultOptions = {
+        RGB = true;
+        RRGGBB = true;
+        names = true;
+        RRGGBBAA = true;
+        rgb_fn = true;
+        hsl_fn = true;
+        css = true;
+        css_fn = true;
+      };
+    };
+  };
+}
