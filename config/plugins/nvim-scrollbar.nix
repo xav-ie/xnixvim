d111d98b8b6f5f62801f0f32c870ebb47c86c4ba
xav-ie <github@xav.ie>
2024-10-20 00:01:01 -0400

    fix(scrollbar): use nvim-scrollbar instead
    It actually works properly and has better preceived UI performance.

--- config/plugins/nvim-scrollbar.nix
+++ config/plugins/nvim-scrollbar.nix
@@ -1,13 +1,21 @@
 { pkgs, ... }:
 {
-  # https://github.com/petertriho/nvim-scrollbar
+  # https://github.com/dstein64/nvim-scrollview
   config = {
     extraPlugins = with pkgs.vimPlugins; [
       nvim-scrollview
     ];
+    extraConfigLua = # lua
+      ''
+        vim.g.scrollview_winblend = 80
+        vim.g.scrollview_winblend_gui = 80
+        require('scrollview').setup({
+          signs_on_startup = {},
+        })
+      '';
     highlight = {
       ScrollView = {
-        bg = "#333333";
+        bg = "White";
       };
     };
   };
