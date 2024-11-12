fc60a2abb84345cb567c99b38154a76ff1fe2af1
xav-ie <github@xav.ie>
2024-11-12 10:49:42 -0500

    chore(update): flake

--- config/plugins/noice.nix
+++ config/plugins/noice.nix
@@ -6,26 +6,28 @@
   config = {
     plugins.noice = {
       enable = true;
-      messages = {
-        view = "mini"; # too many info notifications...very annoying!
-        # I think these two are good candidates for notifications:
-        viewError = "notify";
-        viewWarn = "notify";
-      };
-      lsp.override = {
-        "vim.lsp.util.convert_input_to_markdown_lines" = true;
-        "vim.lsp.util.stylize_markdown" = true;
-        "cmp.entry.get_documentation" = true;
-      };
-      # faulty checks
-      health.checker = false;
+      settings = {
+        messages = {
+          view = "mini"; # too many info notifications...very annoying!
+          # I think these two are good candidates for notifications:
+          viewError = "notify";
+          viewWarn = "notify";
+        };
+        lsp.override = {
+          "vim.lsp.util.convert_input_to_markdown_lines" = true;
+          "vim.lsp.util.stylize_markdown" = true;
+          "cmp.entry.get_documentation" = true;
+        };
+        # faulty checks
+        health.checker = false;
 
-      presets = {
-        bottom_search = true;
-        command_palette = true;
-        long_message_to_split = true;
-        inc_rename = true;
-        lsp_doc_border = true;
+        presets = {
+          bottom_search = true;
+          command_palette = true;
+          long_message_to_split = true;
+          inc_rename = true;
+          lsp_doc_border = true;
+        };
       };
     };
   };
