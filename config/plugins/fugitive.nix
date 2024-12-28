ff840d6e6044423be34d9b2411b153883229adf1
Xavier Ruiz <github@xav.ie>
2024-12-28 10:08:54 -0500

    feat(<leader>g): open :Git

---
+++ config/plugins/fugitive.nix
@@ -0,0 +1,19 @@
+{ ... }:
+{
+  config = {
+    extraConfigLua = # lua
+      ''
+        vim.keymap.set('n', '<leader>g', function()
+          if vim.fn.exists(':Git') > 0 then -- Ensure :Git command is available
+            vim.cmd('Git')                  -- Call :Git command
+          else
+            vim.notify('vim-fugitive is not loaded', vim.log.levels.ERROR)
+          end
+        end, { desc = "Git" })
+      '';
+    # unstoppable git plugin
+    # https://github.com/tpope/vim-fugitive/
+    # https://nix-community.github.io/nixvim/plugins/fugitive
+    plugins.fugitive.enable = true;
+  };
+}
