f543e36ad5e70db85309a2a2d3682700a6127096
xav-ie <github@xav.ie>
2025-01-08 11:51:58 -0500

    chore(flake): cleanup + update

--- flake.nix
+++ flake.nix
@@ -6,85 +6,60 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixvim.url = "github:nix-community/nixvim";
     treefmt-nix.url = "github:numtide/treefmt-nix";
+    systems.url = "github:nix-systems/default";
     # neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
 
     # vendored
-    coq-lsp.url = "github:tomtomjhj/coq-lsp.nvim";
     coq-lsp.flake = false;
-
-    coqtail.url = "github:whonore/Coqtail";
+    coq-lsp.url = "github:tomtomjhj/coq-lsp.nvim";
     coqtail.flake = false;
-
-    oatmeal.url = "github:dustinblackman/oatmeal.nvim";
+    coqtail.url = "github:whonore/Coqtail";
     oatmeal.flake = false;
-
-    octo-nvim.url = "github:pwntester/octo.nvim";
+    oatmeal.url = "github:dustinblackman/oatmeal.nvim";
     octo-nvim.flake = false;
-
-    oil-git-status.url = "github:refractalize/oil-git-status.nvim";
+    octo-nvim.url = "github:pwntester/octo.nvim";
     oil-git-status.flake = false;
-
-    org-roam-nvim.url = "github:chipsenkbeil/org-roam.nvim";
+    oil-git-status.url = "github:refractalize/oil-git-status.nvim";
     org-roam-nvim.flake = false;
-
-    orgmode.url = "github:nvim-orgmode/orgmode";
+    org-roam-nvim.url = "github:chipsenkbeil/org-roam.nvim";
     orgmode.flake = false;
-
-    schemastore-nvim.url = "github:b0o/SchemaStore.nvim";
+    orgmode.url = "github:nvim-orgmode/orgmode";
     schemastore-nvim.flake = false;
-
-    tabscope-nvim.url = "github:backdround/tabscope.nvim";
+    schemastore-nvim.url = "github:b0o/SchemaStore.nvim";
     tabscope-nvim.flake = false;
-
-    vim-guise.url = "github:lambdalisue/vim-guise";
+    tabscope-nvim.url = "github:backdround/tabscope.nvim";
     vim-guise.flake = false;
-
-    witt-neovim.url = "github:typed-rocks/witt-neovim";
+    vim-guise.url = "github:lambdalisue/vim-guise";
     witt-neovim.flake = false;
+    witt-neovim.url = "github:typed-rocks/witt-neovim";
   };
 
   outputs =
-    {
-      nixvim,
-      flake-parts,
-      # neovim-nightly-overlay,
-      ...
-    }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      # TODO: copy this excellent nixvim config:
-      # https://github.com/asungy/nixim/blob/979643e6a13d1803915b379a0e974b86cbcd77ea/flake.nix
-      systems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
 
-      imports = [
-        inputs.treefmt-nix.flakeModule
-      ];
+      imports = [ inputs.treefmt-nix.flakeModule ];
 
       perSystem =
         { pkgs, system, ... }:
         let
-          nixvimLib = nixvim.lib.${system};
-          nixvim' = nixvim.legacyPackages.${system};
+          nixvimLib = inputs.nixvim.lib.${system};
+          nixvim' = inputs.nixvim.legacyPackages.${system};
           nixvimModule = {
             inherit pkgs;
             module = import ./config; # import the module directly
-            # You can use `extraSpecialArgs` to pass additional arguments to your module files
-            # idk how to use this
+            # You can use `extraSpecialArgs` to pass additional arguments to
+            # your module files
             extraSpecialArgs = {
-              inherit inputs;
-              # inherit neovim-nightly-overlay;
-              inherit system;
+              inherit inputs system; # neovim-nightly-overlay;
             };
           };
           nvim = nixvim'.makeNixvimWithModule nixvimModule;
         in
         {
           checks = {
-            # Run `nix flake check .` to verify that your config is not broken
+            # Run `nix flake check` to verify that your config is not broken
             default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
             # TODO: add `nix run github:fzakaria/nix-auto-follow -- -c`
           };
