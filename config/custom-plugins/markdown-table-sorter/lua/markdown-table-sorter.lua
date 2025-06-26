c42710f57f5b948caafb85716dd77d7197823d02
Xavier Ruiz <github@xav.ie>
2025-06-26 10:39:02 -0400

    refactor(markdown-table-sorter): fix lints, refactor into proper module

--- config/custom-plugins/markdown-table-sorter/lua/markdown-table-sorter.lua
+++ config/custom-plugins/markdown-table-sorter/lua/markdown-table-sorter.lua
@@ -1,23 +1,15 @@
+local M = {}
+
 local ts_utils = require("nvim-treesitter.ts_utils")
 
--- Function to log node information
--- local function log_node_info(node, description)
---   local row, col = node:start()
---   local end_row, end_col = node:end_()
---   local type = node:type()
---   print(string.format("%s: Type=%s, Range=[(%d, %d) - (%d, %d)]", description, type, row, col, end_row, end_col))
--- end
-
 -- Function to sort the markdown table by the current column
-function sort_markdown_table_by_current_column(reverse)
+function M.sort_markdown_table_by_current_column(reverse)
 	local current_node = ts_utils.get_node_at_cursor()
 
-	if current_node:type() ~= "inline" then
+	if current_node == nil or current_node:type() ~= "inline" then
 		print("Node is not an inline node, exiting.")
 		return
 	end
-
-	-- log_node_info(current_node, "Initial inline node")
 
 	-- Save current column position
 	local col_position = vim.fn.col(".")
@@ -39,8 +31,6 @@ function sort_markdown_table_by_current_column(reverse)
 		print("No table row found, exiting.")
 		return
 	end
-
-	-- log_node_info(row_node, "Table row node")
 
 	-- Get the column cells for the current row
 	local column_cells = ts_utils.get_named_children(row_node)
@@ -65,7 +55,6 @@ function sort_markdown_table_by_current_column(reverse)
 	-- Find the table parent
 	local parent = row_node:parent()
 	while parent and parent:type() ~= "pipe_table" do
-		-- log_node_info(parent, "Parent node")
 		parent = parent:parent()
 	end
 
@@ -73,8 +62,6 @@ function sort_markdown_table_by_current_column(reverse)
 		print("No table parent found")
 		return
 	end
-
-	-- log_node_info(parent, "Table parent node")
 
 	-- Collect table rows
 	local rows = {}
@@ -86,19 +73,16 @@ function sort_markdown_table_by_current_column(reverse)
 
 	-- Sort the rows based on the column index
 	table.sort(rows, function(a, b)
-		local field_a = ts_utils.get_node_text(a:named_child(col_index - 1)) or ""
-		local field_b = ts_utils.get_node_text(b:named_child(col_index - 1)) or ""
+		local field_a_arr = ts_utils.get_node_text(a:named_child(col_index - 1)) or ""
+		local field_b_arr = ts_utils.get_node_text(b:named_child(col_index - 1)) or ""
 
 		-- Ensure that field_a and field_b are strings
-		field_a = tostring(field_a[1])
-		field_b = tostring(field_b[1])
+		local field_a = tostring(field_a_arr[1])
+		local field_b = tostring(field_b_arr[1])
 
 		-- Check if the fields are numeric
 		local num_a = tonumber(field_a)
 		local num_b = tonumber(field_b)
-
-		-- Log the values being compared
-		-- print(string.format("Comparing: field_a='%s', field_b='%s'", field_a, field_b))
 
 		if num_a and num_b then
 			-- Numeric comparison
@@ -134,7 +118,11 @@ function sort_markdown_table_by_current_column(reverse)
 	vim.fn.setline(start_line, new_lines)
 end
 
--- Command to sort the table
-vim.cmd([[
-          command! -nargs=1 SortTable lua sort_markdown_table_by_current_column(<f-args>)
-        ]])
+function M.setup()
+	-- Command to sort the table
+	vim.api.nvim_create_user_command("SortTable", function(opts)
+		require("markdown-table-sort").sort_markdown_table_by_current_column(opts.args)
+	end, { nargs = 1 })
+end
+
+return M
