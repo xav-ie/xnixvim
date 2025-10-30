cb36123371fe86df775f4bdaf03ea1f569b6393e
Xavier Ruiz <github@xav.ie>
2025-10-29 21:46:02 -0400

    feat(colorizer): add oklch support
    Wait for https://github.com/catgoose/nvim-colorizer.lua/pull/160 to
    merge to be rid of fork.

--- config/plugins/nvim-colorizer.nix
+++ config/plugins/nvim-colorizer.nix
@@ -1,24 +1,34 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   # colors in Neovim
-  # https://github.com/norcalli/nvim-colorizer.lua
-  # https://nix-community.github.io/nixvim/plugins/nvim-colorizer
+  # https://github.com/catgoose/nvim-colorizer.lua/
+  # https://nix-community.github.io/nixvim/plugins/colorizer
   config = {
     plugins.colorizer = {
       enable = true;
+      package = pkgs.vimPlugins.nvim-colorizer-lua.overrideAttrs (_: {
+        src = pkgs.fetchFromGitHub {
+          owner = "xav-ie";
+          repo = "nvim-colorizer.lua";
+          rev = "454acbb74b3220dd26e5703b948e34573080a9ee";
+          sha256 = "sha256-1faAhmHrYhCyeVc4vRaRWC1OW38POjTEwORjtnRdoV4=";
+        };
+      });
       lazyLoad.settings.event = "BufEnter";
       lazyLoad.enable = config.lazyLoad.enable;
       settings = {
         user_default_options = {
-          RGB = true;
-          RRGGBB = true;
-          names = true;
-          RRGGBBAA = true;
-          rgb_fn = true;
-          hsl_fn = true;
           css = true;
-          css_fn = true;
+          AARRGGBB = true;
+          sass = {
+            enable = true;
+            parsers = [ "css" ];
+          };
           tailwind = true;
+          tailwind_opts = {
+            update_names = true;
+          };
+          xterm = true;
         };
       };
     };
